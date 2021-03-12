25f6ff948d193493cdcff83f758a65339a69e374
Pushkar DK <pushkar100@gmail.com>
2021-03-12 08:46:19 +0530

    feat: Phase 1 submit, create, & store tasks

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,19 +1,23 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, map, tap, ignoreElements } from 'rxjs/operators';
+import { filter, mergeMap, map, tap, ignoreElements } from 'rxjs/operators';
+import { from } from 'rxjs';
 import { isActionOf } from 'typesafe-actions';
 
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
+import { WorkloadService } from './services'
 
+const workloadService = new WorkloadService
 
 type AppEpic = Epic<RootAction, RootAction, RootState>;
 
 const logWorkloadSubmissions: AppEpic = (action$, state$) => (
   action$.pipe(
     filter(isActionOf(workloadsActions.submit)),
-    map(action => action.payload),
-    tap((payload) => console.log('Workload submitted', payload)),
-    ignoreElements(),
+    mergeMap(async (action) => {
+      const createdTask = await workloadService.create(action.payload)
+      return workloadsActions.created(createdTask)
+    })
   )
 );
 
